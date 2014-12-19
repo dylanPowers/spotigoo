915fcf34a085a9f756c12d5debb767dd3152baf7
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-12-19 07:12:41 -0800

    Reimplementation of user info grabbing with the new spotify wrapper library

--- spotigoo-music-migration/src/main/java/dylankpowers/spotigoo/MigrationService.java
+++ spotigoo-music-migration/src/main/java/dylankpowers/spotigoo/MigrationService.java
@@ -1,17 +1,16 @@
 package dylankpowers.spotigoo;
 
+import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
-import com.android.volley.*;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import kaaes.spotify.webapi.android.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import kaaes.spotify.webapi.android.models.User;
+import retrofit.Callback;
+import retrofit.RetrofitError;
 
 public class MigrationService extends Service {
     final MigrationBinder mBinder = new MigrationBinder();
@@ -28,48 +27,25 @@ public class MigrationService extends Service {
     }
 
     public void getUserInfo(String spotifyAccessToken, final UserInfoCallback callback) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.spotify.com/v1/me";
-        SpotifyRequest request = new SpotifyRequest(
-                url,
-                new Response.Listener<String>() {
+        SpotifyApi spotifyApi = new SpotifyApi();
+        spotifyApi.setAccessToken(spotifyAccessToken);
+        SpotifyService spotifyService = spotifyApi.getService();
+
+        spotifyService.getMe(new Callback<User>() {
+            @Override
+            public void success(User user, retrofit.client.Response response) {
+               Log.d("Spotify request", user.display_name);
+            }
 
             @Override
-            public void onResponse(String response) {
-                callback.call(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+            public void failure(RetrofitError error) {
                 Log.e("Spotify request", "Spotify request failed");
             }
         });
-
-        try {
-            request.getHeaders().put("Authorization", "Bearer " + spotifyAccessToken);
-        } catch (AuthFailureError e) {
-            Log.e("Spotify request", "failed setting the authorization header");
-        }
-
-        queue.add(request);
     }
 
     public interface UserInfoCallback {
         public void call(String userInfo);
-    }
-
-    public class SpotifyRequest extends StringRequest {
-        private HashMap<String, String> mHeaders = new HashMap<String, String>();
-
-        SpotifyRequest(String url, Response.Listener<String> listener,
-                       Response.ErrorListener errorListener) {
-            super(url, listener, errorListener);
-        }
-
-        @Override
-        public Map<String, String> getHeaders() throws AuthFailureError {
-            return mHeaders;
-        }
     }
 }
 
